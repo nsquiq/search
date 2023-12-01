ed641ed3b8275b9dd9def998c19c50e707276aa9
Nuray <210103331@stu.sdu.edu.kz>
2023-12-01 11:36:10 +0600

    network

--- app/src/main/java/com/example/bookshelf/data/DefaultBookshelfRepository.kt
+++ app/src/main/java/com/example/bookshelf/data/DefaultBookshelfRepository.kt
@@ -1,2 +1,39 @@
 package com.example.bookshelf.data
 
+
+import com.example.bookshelf.model.Book
+import com.example.bookshelf.network.BookshelfApiService
+
+class DefaultBookshelfRepository (
+    private val bookshelfApiService: BookshelfApiService
+) : BookshelfRepository {
+
+
+    override suspend fun getBooks(query: String): List<Book>? {
+        return try {
+            val res = bookshelfApiService.getBooks(query)
+            if (res.isSuccessful) {
+                res.body()?.items ?: emptyList()
+            } else {
+                emptyList()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    override suspend fun getBook(id: String): Book? {
+        return try {
+            val res = bookshelfApiService.getBook(id)
+            if (res.isSuccessful) {
+                res.body()
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
